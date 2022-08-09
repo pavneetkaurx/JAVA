85cff1906705d599c977b9095be89fa72c10ef89
Pavneet Kaur <pavneetkaur58@gmail.com>
2022-08-09 18:44:06 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    //sum of three definite numbers
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 5;
+        int num3 = 7;
+        int sum = num1 + num2 + num3;
+        System.out.print("sum is:");
+        System.out.println(sum);
+    }
+}
